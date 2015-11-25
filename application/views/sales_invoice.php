47c7727829e17fe633367fd889aef142215b8369
Paul Christian Rueda <chrisrueda14@yahoo.com>
2015-11-25 15:53:26 -0800

    Test Commit.

--- application/views/sales_invoice.php
+++ application/views/sales_invoice.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>JCore v2</title>
+    <title>PAUL CHRISTIAN RUEDA</title>
 
 	
     <?php include('assets/includes/global_css.html'); ?>
